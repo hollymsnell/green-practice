f82d13a86986c2f30f499a2e07e0bc8afe365962
hollymsnell <hollymsnell@gmail.com>
2022-09-10 12:33:25 -0600

    if/else two thru 4

--- practice.rb
+++ practice.rb
@@ -1,8 +1,38 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-num = 6
-if num == 10
-  p 0
-else
-  p -1
-end
+# num = 6
+# if num == 10
+#   p 0
+# else
+#   p -1
+# end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# num = 10
+# if num < 10
+#   p -1
+# elsif num > 10 
+#   p 1
+# else
+#   p 0
+# end
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+# num1 = 3
+# num2 = 65
+# if num1 && num2 < 10
+#   p 1
+# else 
+#   p 0
+# end
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+# num = 67
+# if num > 9000
+#   p 1
+# else p -1
+# end
+
