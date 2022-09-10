9017a2c54e8de8b187ad2105b3ff665368a09808
hollymsnell <hollymsnell@gmail.com>
2022-09-10 12:49:40 -0600

    js if/else 2-4

--- practice.js
+++ practice.js
@@ -2,9 +2,39 @@
 
 // 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-var number = 10;
-if (number === 10) {
-  console.log(0);
+// var number = 10;
+// if (number === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
+
+//2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+// var number = 10;
+// if (number < 10) {
+//   console.log(-1);
+// } else if (number > 10) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+// 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+// var num1 = 3;
+// var num2 = 5;
+// if (num1 && num2 < 10) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+// 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+var number = 89;
+if (number > 9000) {
+  console.log(1);
 } else {
   console.log(-1);
 }
