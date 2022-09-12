10ec46b6e83bd005f0b5fd8b5d905edde0c603c0
hollymsnell <hollymsnell@gmail.com>
2022-09-11 19:28:23 -0600

    interp/concant for js and rb problems 1-5 on skills.

--- practice.js
+++ practice.js
@@ -32,9 +32,42 @@
 
 // 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-var number = 89;
-if (number > 9000) {
-  console.log(1);
-} else {
-  console.log(-1);
-}
+// var number = 89;
+// if (number > 9000) {
+//   console.log(1);
+// } else {
+//   console.log(-1);
+// }
+
+// # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+var first = "holly";
+var last = "snell";
+console.log(first + " " + last);
+
+// # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+var firstName = "Joe";
+var lastName = "Satriani";
+console.log(`${first} ${last}`);
+
+// # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// var word = window.prompt("Enter a word:");
+// if (word === "marco") {
+//   console.log("polo");
+// }
+
+// # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+var col1 = "yellow";
+var col2 = "red";
+var col3 = "blue";
+console.log("Primary colors include " + col1 + ", " + col2 + ", and " + col3 + ".");
+
+// # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+var col1 = "yellow";
+var col2 = "red";
+var col3 = "blue";
+console.log(`Primary colors include ${col1}, ${col2}, and ${col3}.`);
--- practice.rb
+++ practice.rb
@@ -38,3 +38,40 @@
 # else p -1
 # end
 
+
+
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first = "holly"
+last = "snell"
+p first + " " + last
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first = "John"
+last = "Lennon"
+p "#{first} #{last}"
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+puts "Enter a word: "
+word = gets.chomp
+if word == "marco"
+  p "polo"
+else 
+  p "nice"
+end
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+col1 = "blue"
+col2 = "yellow"
+col3 = "red"
+p "The primary colors to create all the others are " + col1 + ", " + col2 + ", and " + col3 + "!"
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+col1 = "blue"
+col2 = "yellow"
+col3 = "red"
+p "The primary colors to create all the others are #{col1}, #{col2}, and #{col3}!" 
