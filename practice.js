99a7f9696b2fc7293a557b582c736353f86ff7ed
hollymsnell <hollymsnell@gmail.com>
2022-09-12 20:47:16 -0600

    js methods with error for window

--- practice.js
+++ practice.js
@@ -1,1 +1,14 @@
 //node practice.js ***
+
+// # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+// var word = window.prompt("Enter a word: ");
+// console.log(word.toUpperCase());
+
+// # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+// # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+// 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
