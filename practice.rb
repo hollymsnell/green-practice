bf597d36e49253c7fcfe39544e5990bed2895003
hollymsnell <hollymsnell@gmail.com>
2022-09-12 20:29:02 -0600

    built in methods ruby number 1-4 and 10 on skills

--- practice.rb
+++ practice.rb
@@ -1,77 +1,36 @@
-#ruby practice.rb
+#ruby practice.rb ***
 
-# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-# num = 6
-# if num == 10
-#   p 0
-# else
-#   p -1
+# puts "Enter a word:"
+# word = gets.chomp
+# p word.upcase 
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# puts "Enter a number:"
+# number = gets.chomp
+# if number.to_i > 100
+#   p "That's a big number"
 # end
 
-# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
-# num = 10
-# if num < 10
-#   p -1
-# elsif num > 10 
-#   p 1
-# else
-#   p 0
+# puts "Enter two numbers:"
+# num1 = gets.chomp
+# num2 = gets.chomp
+# p num1.to_i + num2.to_i
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# puts "Enter a word:"
+# word = gets.chomp
+# p word.reverse
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+# puts "Enter a word"
+# word = gets.chomp
+# if word.length > 5
+#   p "That's a long word"
 # end
-
-# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-
-# num1 = 3
-# num2 = 65
-# if num1 && num2 < 10
-#   p 1
-# else 
-#   p 0
-# end
-
-# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-
-# num = 67
-# if num > 9000
-#   p 1
-# else p -1
-# end
-
-
-
-# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
-
-first = "holly"
-last = "snell"
-p first + " " + last
-
-# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
-
-first = "John"
-last = "Lennon"
-p "#{first} #{last}"
-
-# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
-
-puts "Enter a word: "
-word = gets.chomp
-if word == "marco"
-  p "polo"
-else 
-  p "nice"
-end
-
-# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
-
-col1 = "blue"
-col2 = "yellow"
-col3 = "red"
-p "The primary colors to create all the others are " + col1 + ", " + col2 + ", and " + col3 + "!"
-
-# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
-
-col1 = "blue"
-col2 = "yellow"
-col3 = "red"
-p "The primary colors to create all the others are #{col1}, #{col2}, and #{col3}!" 
