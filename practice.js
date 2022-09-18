2bf2e896c2bea0fcb9eb84305148dd5ca0a6bc9e
hollymsnell <hollymsnell@gmail.com>
2022-09-17 19:49:24 -0600

    6-10 JS practice hashes

--- practice.js
+++ practice.js
@@ -41,10 +41,35 @@ console.log(books[2].author);
 
 // # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
+var capitals = { michigan: "lansing", florida: "tallahassee", nevada: "reno" };
+capitals["louisiana"] = "baton rouge";
+console.log(capitals);
+
 // # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+var laptop = { brand: "apple", model: "macbook air", year: "2022" };
+console.log(laptop.brand);
+console.log(laptop.model);
+console.log(laptop.year);
 
 // # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
+var laptops = [
+  { brand: "apple", model: "macbook air", year: "2022" },
+  { brand: "toshiba", model: "something", year: "2022" },
+  { brand: "apple", model: "macbook pro", year: "2022" },
+];
+console.log(laptops[1].model);
+
 // # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
+var definition = { concur: "to agree with", highfive: "to slap hands with another as a form of celebration or unity" };
+definition["cat"] = "one of the wildest domesticated land animals";
+console.log(definition);
+
 // # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+var shirt = { brand: "hanes", color: "blue", size: "medium" };
+console.log(shirt.brand);
+console.log(shirt.color);
+console.log(shirt.size);
