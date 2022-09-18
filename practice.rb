2296cf77ae59648aeabcfebfbb71a479c69b299e
hollymsnell <hollymsnell@gmail.com>
2022-09-17 19:22:00 -0600

    6-10 ruby hash practice

--- practice.rb
+++ practice.rb
@@ -43,10 +43,35 @@ p books[2]["author"]
 
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
+capitals = {"colorado" => "denver", "kansas" => "topeka", "texas" => "austin"}
+capitals["idaho"] = "boise"
+p capitals
+
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+laptop = {"brand" => "apple", "model" => "air", "year" => 2022}
+p laptop["brand"]
+p laptop["model"]
+p laptop["year"]
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
+laptops = [
+  {"brand" => "apple", "model" => "air", "year" => 2022},
+  {"brand" => "sony", "model" => "silly", "year" => 2022},
+  {"brand" => "toshiba", "model" => "tushy", "year" => 2022}
+]
+p laptops[1]["model"] 
+
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
+definition = {"concur" => "to agree with", "high-five" => "to slap hands with another as a form of celebration or unity"}
+definition["cat"] = "one of the wildest domesticated land animals"
+p definition
+
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+shirt = {"brand" => "vans", "color" => "yellow", "size" => "small"}
+p shirt["brand"]
+p shirt["color"]
+p shirt["size"]
