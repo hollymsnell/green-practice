f1d5577fe04ae45479c0068e0a4144ac3a00671f
hollymsnell <hollymsnell@gmail.com>
2022-10-03 21:14:21 -0600

    ruby 6-10 skills checklist conditioned arrays .select

--- practice.rb
+++ practice.rb
@@ -341,14 +341,48 @@ p short_words
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+short_names = hashes.select do |hash|
+  hash[:name].length < 6
+end
+p short_names
+
+
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+numbers = [8, 23, 0, 44, 1980, 3]
+ten_less = numbers.select do |number|
+  number < 10
+end
+p ten_less
+
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
+strings = ["big", "little", "good", "bad"]
+no_b_words = strings.select do |string|
+  string[0] != "b"
+end
+p no_b_words
+
+
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+low_price = hashes.select do |hash|
+  hash[:price] < 10
+end
+p low_price
+
+
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+numbers = [2, 4, 5, 1, 8, 9, 7] 
+odd_numbers = numbers.select do |number|
+  number % 2 == 1
+end
+p odd_numbers
