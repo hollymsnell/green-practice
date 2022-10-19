2c51687969089eb94be1d8340d59a78d871d58de
hollymsnell <hollymsnell@gmail.com>
2022-10-18 20:22:26 -0600

    reduce arrays SC 6-10 ruby

--- practice.rb
+++ practice.rb
@@ -72,14 +72,71 @@ p lengthsum
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+lowest_price = hashes[0]
+index = 0
+while index < hashes.length
+  price = hashes[index]
+  if price[:price] < lowest_price[:price]
+    lowest_price = price
+  end
+  index = index + 1
+end
+p lowest_price
+
+
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
+
+numbers = [5, 10, 8, 3]
+product = 1
+index = 0
+while index < numbers.length
+  product = product * numbers[index]
+  index = index + 1
+end
+p product
+
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
+strings = ["volleyball", "basketball", "badminton"]
+dashes = "-"
+index = 0
+while index < strings.length
+  dashes = dashes + strings[index] + "-"
+  index = index + 1
+end
+p dashes
+
+
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+short_name = hashes[0]
+index = 0
+while index < hashes.length
+  shortest = hashes[index]
+  if shortest[:name].length < short_name[:name].length
+    short_name = shortest
+  end
+  index = index + 1
+end
+p short_name
+
+
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
+
+numbers = [5, 10, 8, 3]
+max = 0 
+index = 0
+while index < numbers.length
+  if numbers[index] > max
+    max = numbers[index]
+  end
+  index = index + 1
+end
+p max
