59d65e1eb91250efe9465f4ca7620653ebe43e0b
hollymsnell <hollymsnell@gmail.com>
2022-10-25 19:41:25 -0600

    SC ruby 1-5 .reduce

--- practice.rb
+++ practice.rb
@@ -260,3 +260,74 @@ numbers.each do |number|
   end
 end
 p maximum
+
+
+
+
+###.reduce
+
+#  1. Start with an array of numbers and compute the sum of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 26.
+
+numbers = [5, 10, 8, 3]
+sum = numbers.reduce(0) do |sum, number|
+  sum + number
+end
+p sum
+
+#  2. Start with an array of strings and combine them all into a single string.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+sports = ["volleyball", "basketball", "badminton"]
+string = sports.reduce("") do |string, sport|
+  sport + string
+end
+p string
+
+
+#  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+sum = items.reduce(0) do |sum, item|
+  item[:price] + sum
+end
+p sum
+
+#  4. Start with an array of numbers and compute the the minumum number.
+#     For example, [5, 10, 8, 3, 9] becomes 3.
+
+numbers = [5, 10, 8, 3, 9]
+minumum = numbers.reduce(numbers[0]) do |minumum, number|
+  if number < minumum
+    number
+ else
+  minumum
+  end
+end
+p minumum
+
+#  5. Start with an array of strings and compute the total length of all the strings.
+#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+sports = ["volleyball", "basketball", "badminton"]
+total_length = sports.reduce(0) do |total, sport|
+  total + sport.length
+end
+p total_length
+
+
+#  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+#  7. Start with an array of numbers and compute product of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 1200.
+
+#  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+#  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+# 10. Start with an array of numbers and compute the maximum number.
+#     For example, [5, 10, 8, 3] becomes 10.
