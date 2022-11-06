4db9057b832153aafecb62fd4ed95d4f59b1e064
hollymsnell <hollymsnell@gmail.com>
2022-11-06 12:34:23 -0700

    6-10 JS SC converting data

--- practice.js
+++ practice.js
@@ -73,14 +73,79 @@ console.log(peopleArray);
 // #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 // #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
 
+var wordies = ["do", "or", "do", "not"];
+var veces = {};
+index = 0;
+while (index < wordies.length) {
+  var wordy = wordies[index];
+  if (veces[wordy] === undefined) {
+    veces[wordy] = 0;
+  }
+  veces[wordy] += 1;
+  index++;
+}
+console.log(veces);
+
 // #  7. Convert a hash into a flat array containing all the hash’s keys and values.
 // #     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+
+var hashy = { a: 1, b: 2, c: 3, d: 4 };
+var flatStanley = [];
+Object.entries(hashy).forEach(function ([key, value]) {
+  flatStanley.push(key);
+  flatStanley.push(value);
+});
+console.log(flatStanley);
 
 // #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
 // #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
 
+var priceObject = { chair: 75, book: 15 };
+var cosas = [
+  { name: "chair", color: "red", weight: 10 },
+  { name: "book", color: "black", weight: 1 },
+];
+var combinedObject = {};
+index = 0;
+while (index < cosas.length) {
+  var cosa = cosas[index];
+  var name = cosa.name;
+  var color = cosa.color;
+  var weight = cosa.weight;
+  var price = priceObject[name];
+  combinedObject[name] = { price: price, color: color, weight: weight };
+  index += 1;
+}
+console.log(combinedObject);
+
 // #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 // #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
 
+var books = [
+  { author: "Jeff Smith", title: "Bone" },
+  { author: "George Orwell", title: "1984" },
+  { author: "Jeff Smith", title: "RASL" },
+];
+var booksObject = {};
+index = 0;
+while (index < books.length) {
+  var book = books[index];
+  var author = book.author;
+  var title = book.title;
+  if (booksObject[author] === undefined) {
+    booksObject[author] = [];
+  }
+  booksObject[author].push(title);
+  index = index + 1;
+}
+console.log(booksObject);
+
 // # 10. Given a hash, create a new hash that has the keys and values switched.
 // #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
+
+var OG = { a: 1, b: 2, c: 3 };
+var flipIt = {};
+Object.entries(OG).forEach(function ([key, value]) {
+  flipIt[value] = key;
+});
+console.log(flipIt);
