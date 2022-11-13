823104384332515a6f6c784acfd0fcd15828b0ec
hollymsnell <hollymsnell@gmail.com>
2022-11-13 10:29:39 -0700

    1 and 2 nested loops JS SC

--- practice.js
+++ practice.js
@@ -3,8 +3,41 @@
 // #  1. Use a nested loop to convert an array of number pairs into a single flattened array.
 // #     For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
 
+var buddies = [
+  [1, 3],
+  [8, 9],
+  [2, 16],
+];
+var family = [];
+var index = 0;
+while (index < buddies.length) {
+  var buddy = buddies[index];
+  var index2 = 0;
+  while (index2 < buddy.length) {
+    var bud = buddy[index2];
+    family.push(bud);
+    index2 = index2 + 1;
+  }
+  index = index + 1;
+}
+console.log(family);
+
 // #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 // #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
+
+var letters1 = ["a", "b", "c"];
+var letters2 = ["d", "e", "f", "g"];
+var allLetters = [];
+index = 0;
+while (index < letters1.length) {
+  index2 = 0;
+  while (index2 < letters2.length) {
+    allLetters.push(letters1[index] + letters2[index2]);
+    index2 += 1;
+  }
+  index += 1;
+}
+console.log(allLetters);
 
 // #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 // #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
