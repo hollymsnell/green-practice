2e40193df3ee64988ffcf181609765e2e44a82c5
hollymsnell <hollymsnell@gmail.com>
2023-03-08 18:04:23 -0700

    practice with while loops Ruby

--- practice.rb
+++ practice.rb
@@ -1,76 +1,92 @@
 #ruby practice.rb ***
 
-# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-p "enter a word:"
-word = gets.chomp
-puts word.upcase 
+# 1. Write a while loop to print the numbers 1 through 10.
 
-# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
-
-p "Enter a number:"
-number = gets.chomp.to_i
-if number > 100
-  p "wow that's a big number!"
-else
-  p "baby numby"
+number = 1 
+while number <= 10
+  p number
+  number = number + 1
 end
 
-# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
-p "Please enter two numbers:"
-number1 = gets.chomp
-number2 = gets.chomp
-p number1.to_i + number2.to_i
+# 2. Write a while loop that prints the word "hello" 5 times.
 
-# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
-
-p "Enter a word to be reversed my child:"
-word = gets.chomp
-p word.reverse
-
-# 5. Write a program that asks the user to enter a number, then prints the number times 10.
-
-p "Enter a number, my guy:"
-number = gets.chomp
-p number.to_i * 10
-
-# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
-
-p "Let's get two words from you:"
-word1 = gets.chomp
-word2 = gets.chomp
-p "#{word1} #{word2}".upcase
-
-# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
-
-p "Can you just enter a word and i'll count it:"
-word = gets.chomp
-p word.length
-
-# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
-
-p "Enter a number, please:"
-number = gets.chomp
-if number.to_i < 0
-  puts "That's a negative number."
-else
-  puts "thanskss"
+count = 0
+while count < 5
+  p "hello"
+  count = count + 1
 end
 
-# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+array = []
+while true
+p "Enter a word:"
+  word = gets.chomp
+  array << word
+  p array
+  if word == "stop"
+    break
+  end 
+end
 
-p "Enter two numbies babyyy:"
-number1 = gets.chomp
-number2 = gets.chomp
-p number1.to_i * number2.to_i
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+number = 0
+while number <= 100
+  p number
+  number = number + 5
+end
 
-p "Enter a word, gosh:"
-word = gets.chomp
-if word.to_s.length > 5
-  puts "That's a long word."
-else
-  puts "baby word"
+# 5. Write a while loop that prints the number 9000 ten times.
+
+count = 0
+while count < 10
+  p 9000
+  count = count + 1
+end
+
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+while true
+  p "enter a number"
+  number = gets.chomp
+  if number.to_i > 10
+    break
+  end
+end
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+number = 50
+while number <= 70
+  p number
+  number = number + 1
+end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+count = 0
+while count < 144
+  p "Around the World"
+  count = count + 1
+end
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+while true
+  p "enter a word"
+  input = gets.chomp
+  if input.length > 5
+    break
+  end
+end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+number = 2
+while number <= 40
+  p number
+  number = number + 2
 end
