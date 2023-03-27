eed5958334b7e93da7f3134a070502c1b4b0f447
hollymsnell <hollymsnell@gmail.com>
2023-03-27 09:14:11 -0600

    reduce arrays .each ruby

--- practice.js
+++ practice.js
@@ -5,148 +5,150 @@
 // #  1. Start with an array of numbers and compute the sum of all the numbers.
 // #     For example, [5, 10, 8, 3] becomes 26.
 
-var numbers = [2, 6, 34, 6];
-var index = 0;
-var sum = 0;
-while (index < numbers.length) {
-  sum = sum + numbers[index];
-  index += 1;
-}
-console.log(sum);
+// var numbers = [2, 6, 34, 6];
+// var index = 0;
+// var sum = 0;
+// while (index < numbers.length) {
+//   sum = sum + numbers[index];
+//   index += 1;
+// }
+// console.log(sum);
 
-// #  2. Start with an array of strings and combine them all into a single string.
-// #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+// // #  2. Start with an array of strings and combine them all into a single string.
+// // #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
-var strings = ["winter", "spring", "summer", "fall"];
-index = 0;
-var longBoy = "";
-while (index < strings.length) {
-  longBoy = longBoy + strings[index];
-  index += 1;
-}
-console.log(longBoy);
+// var strings = ["winter", "spring", "summer", "fall"];
+// index = 0;
+// var longBoy = "";
+// while (index < strings.length) {
+//   longBoy = longBoy + strings[index];
+//   index += 1;
+// }
+// console.log(longBoy);
 
-// #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
-// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+// // #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+// // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
-var furniture = [
-  { name: "bed", price: 100 },
-  { name: "lamp", price: 1 },
-  { name: "stool", price: 4 },
-  { name: "plant", price: 7 },
-  { name: "dresser", price: 50 },
-];
-var priceSum = 0;
-index = 0;
-while (index < furniture.length) {
-  priceSum = priceSum + furniture[index].price;
-  index += 1;
-}
-console.log(priceSum);
+// var furniture = [
+//   { name: "bed", price: 100 },
+//   { name: "lamp", price: 1 },
+//   { name: "stool", price: 4 },
+//   { name: "plant", price: 7 },
+//   { name: "dresser", price: 50 },
+// ];
+// var priceSum = 0;
+// index = 0;
+// while (index < furniture.length) {
+//   priceSum = priceSum + furniture[index].price;
+//   index += 1;
+// }
+// console.log(priceSum);
 
-// #  4. Start with an array of numbers and compute the the minumum number.
-// #     For example, [5, 10, 8, 3, 9] becomes 3.
+// // #  4. Start with an array of numbers and compute the the minumum number.
+// // #     For example, [5, 10, 8, 3, 9] becomes 3.
 
-var values = [4, 23, 134, 5];
-var minumum = values[0];
-index = 0;
-while (index < values.length) {
-  if (values[index] < minumum) {
-    minumum = values[index];
-  }
-  index += 1;
-}
-console.log(minumum);
+// var values = [4, 23, 134, 5];
+// var minumum = values[0];
+// index = 0;
+// while (index < values.length) {
+//   if (values[index] < minumum) {
+//     minumum = values[index];
+//   }
+//   index += 1;
+// }
+// console.log(minumum);
 
-// #  5. Start with an array of strings and compute the total length of all the strings.
-// #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+// // #  5. Start with an array of strings and compute the total length of all the strings.
+// // #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
 
-var words = ["happy", "sad", "angry", "glad"];
-var totalLength = 0;
-index = 0;
-while (index < words.length) {
-  totalLength = totalLength + words[index].length;
-  index += 1;
-}
-console.log(totalLength);
+// var words = ["happy", "sad", "angry", "glad"];
+// var totalLength = 0;
+// index = 0;
+// while (index < words.length) {
+//   totalLength = totalLength + words[index].length;
+//   index += 1;
+// }
+// console.log(totalLength);
 
-// #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
-// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+// // #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+// // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
-var things = [
-  { name: "bed", price: 100 },
-  { name: "lamp", price: 1 },
-  { name: "stool", price: 4 },
-  { name: "plant", price: 7 },
-  { name: "dresser", price: 50 },
-];
-var lowest = things[0];
-index = 0;
-while (index < things.length) {
-  if (things[index].price < lowest.price) {
-    lowest = things[index];
-  }
-  index += 1;
-}
-console.log(lowest);
+// var things = [
+//   { name: "bed", price: 100 },
+//   { name: "lamp", price: 1 },
+//   { name: "stool", price: 4 },
+//   { name: "plant", price: 7 },
+//   { name: "dresser", price: 50 },
+// ];
+// var lowest = things[0];
+// index = 0;
+// while (index < things.length) {
+//   if (things[index].price < lowest.price) {
+//     lowest = things[index];
+//   }
+//   index += 1;
+// }
+// console.log(lowest);
 
-// #  7. Start with an array of numbers and compute product of all the numbers.
-// #     For example, [5, 10, 8, 3] becomes 1200.
+// // #  7. Start with an array of numbers and compute product of all the numbers.
+// // #     For example, [5, 10, 8, 3] becomes 1200.
 
-var math = [5, 23, 11, 2];
-var product = 1;
-index = 0;
-while (index < math.length) {
-  product = product * math[index];
-  index += 1;
-}
-console.log(product);
+// var math = [5, 23, 11, 2];
+// var product = 1;
+// index = 0;
+// while (index < math.length) {
+//   product = product * math[index];
+//   index += 1;
+// }
+// console.log(product);
 
-// #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
-// #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+// // #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+// // #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
-var seasons = ["winter", "spring", "summer", "fall"];
-var singleString = "-";
-index = 0;
-while (index < seasons.length) {
-  singleString = singleString + `${seasons[index]}-`;
-  index += 1;
-}
-console.log(singleString);
+// var seasons = ["winter", "spring", "summer", "fall"];
+// var singleString = "-";
+// index = 0;
+// while (index < seasons.length) {
+//   singleString = singleString + `${seasons[index]}-`;
+//   index += 1;
+// }
+// console.log(singleString);
 
-// #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
-// #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+// // #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+// // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 
-var decor = [
-  { name: "bed", price: 100 },
-  { name: "lamp", price: 1 },
-  { name: "stool", price: 4 },
-  { name: "plant", price: 7 },
-  { name: "dresser", price: 50 },
-];
-var shorty = decor[0];
-index = 0;
-while (index < decor.length) {
-  if (decor[index].name.length < shorty.name.length) {
-    shorty = decor[index];
-  }
-  index += 1;
-}
-console.log(shorty);
+// var decor = [
+//   { name: "bed", price: 100 },
+//   { name: "lamp", price: 1 },
+//   { name: "stool", price: 4 },
+//   { name: "plant", price: 7 },
+//   { name: "dresser", price: 50 },
+// ];
+// var shorty = decor[0];
+// index = 0;
+// while (index < decor.length) {
+//   if (decor[index].name.length < shorty.name.length) {
+//     shorty = decor[index];
+//   }
+//   index += 1;
+// }
+// console.log(shorty);
 
-// # 10. Start with an array of numbers and compute the maximum number.
-// #     For example, [5, 10, 8, 3] becomes 10.
+// // # 10. Start with an array of numbers and compute the maximum number.
+// // #     For example, [5, 10, 8, 3] becomes 10.
 
-var digits = [56, 234, 56778, 12, 134];
-var maximum = digits[0];
-index = 0;
-while (index < digits.length) {
-  if (digits[index] > maximum) {
-    maximum = digits[index];
-  }
-  index += 1;
-}
-console.log(maximum);
+// var digits = [56, 234, 56778, 12, 134];
+// var maximum = digits[0];
+// index = 0;
+// while (index < digits.length) {
+//   if (digits[index] > maximum) {
+//     maximum = digits[index];
+//   }
+//   index += 1;
+// }
+// console.log(maximum);
+
+// ###.forEach###
 
 // #  1. Start with an array of numbers and compute the sum of all the numbers.
 // #     For example, [5, 10, 8, 3] becomes 26.
--- practice.rb
+++ practice.rb
@@ -5,26 +5,174 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 26.
 
-numbers = [4, 56, 2]
-index = 0
+# numbers = [4, 56, 2]
+# index = 0
+# sum = 0
+# while index < numbers.length
+#   sum = sum + numbers[index]
+#   index = index + 1
+# end
+# p sum
+
+# #  2. Start with an array of strings and combine them all into a single string.
+# #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+# strings = ["aint", "seen", "nothin", "yet"]
+# index = 0
+# long_boy = ""
+# while index < strings.length
+#   long_boy << strings[index]
+#   index += 1
+# end
+# p long_boy
+
+# #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+# #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+# items = [
+#   {name: "bed", price: 100}, 
+#   {name: "lamp", price: 1}, 
+#   {name: "stool", price: 4},
+#   {name: "plant", price: 7},
+#   {name: "dresser", price: 50}
+# ]
+# index = 0
+# sum = 0
+# while index < items.length
+#   sum = sum + items[index][:price]
+#   index += 1
+# end
+# p sum
+
+# #  4. Start with an array of numbers and compute the the minumum number.
+# #     For example, [5, 10, 8, 3, 9] becomes 3.
+
+# numbers = [34, 1, 45, 3, 14]
+# index = 0
+# min_num = numbers[0]
+# while index < numbers.length
+# if numbers[index] < min_num
+#   min_num = numbers[index]
+# end
+# index += 1
+# end
+# p min_num
+
+# #  5. Start with an array of strings and compute the total length of all the strings.
+# #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+# strings = ["volleyball", "basketball", "badminton"]
+# index = 0
+# total_length = 0
+# while index < strings.length
+#   total_length = total_length + strings[index].length
+#   index += 1
+# end
+# p total_length
+
+# #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+# #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+# items = [
+#   {name: "bed", price: 100}, 
+#   {name: "lamp", price: 1}, 
+#   {name: "stool", price: 4},
+#   {name: "plant", price: 7},
+#   {name: "dresser", price: 50}
+# ]
+# index = 0 
+# lowest_price = items[0][:price]
+# while index < items.length
+#   if items[index][:price] < lowest_price
+#     lowest_price = items[index][:price]
+#   end
+#   index += 1
+# end
+# p lowest_price
+
+# #  7. Start with an array of numbers and compute product of all the numbers.
+# #     For example, [5, 10, 8, 3] becomes 1200.
+
+# numbers = [12, 6, 22, 1]
+# index = 0
+# product = 1
+# while index < numbers.length
+#   product = product * numbers[index]
+#   index += 1
+# end
+# p product
+
+# #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+# #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+# strings = ["summer", "fall", "winter", "spring"]
+# index = 0
+# joined = "-"
+# while index < strings.length
+#   string = strings[index]
+#   joined += "#{string}-"
+#   index += 1
+# end
+# p joined
+
+# #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+# #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+# items = [
+#   {name: "bed", price: 100}, 
+#   {name: "lamp", price: 1}, 
+#   {name: "stool", price: 4},
+#   {name: "plant", price: 7},
+#   {name: "dresser", price: 50}
+# ]
+# shortest_name = items[0]
+# index = 0
+# while index < items.length
+#   if items[index][:name].length < shortest_name[:name].length
+#     shortest_name = items[index][:name]
+#   end
+#   index += 1
+# end
+# p shortest_name
+
+# # 10. Start with an array of numbers and compute the maximum number.
+# #     For example, [5, 10, 8, 3] becomes 10.
+
+# numbers = [5, 234, 66, 12]
+# max_num = numbers[0]
+# index = 0
+# while index < numbers.length
+#   if numbers[index] > max_num
+#     max_num = numbers[index]
+#   end
+#   index += 1
+# end
+# p max_num
+
+
+
+
+###.each###
+
+#  1. Start with an array of numbers and compute the sum of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 26.
+
+numbers = [34, 56, 2, 84]
 sum = 0
-while index < numbers.length
-  sum = sum + numbers[index]
-  index = index + 1
+numbers.each do |number|
+  sum = sum + number
 end
 p sum
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
-strings = ["aint", "seen", "nothin", "yet"]
-index = 0
-long_boy = ""
-while index < strings.length
-  long_boy << strings[index]
-  index += 1
+strings = ["winter", "spring", "summer", "fall"]
+one_string = ""
+strings.each do |string|
+  one_string = one_string + string
 end
-p long_boy
+p one_string
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
@@ -36,39 +184,33 @@ items = [
   {name: "plant", price: 7},
   {name: "dresser", price: 50}
 ]
-index = 0
 sum = 0
-while index < items.length
-  sum = sum + items[index][:price]
-  index += 1
+items.each do |item|
+  sum = sum + item[:price]
 end
 p sum
 
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
 
-numbers = [34, 1, 45, 3, 14]
-index = 0
+numbers = [34, 7, 23, 36, 9]
 min_num = numbers[0]
-while index < numbers.length
-if numbers[index] < min_num
-  min_num = numbers[index]
-end
-index += 1
+numbers.each do |number|
+  if number < min_num
+    min_num = number
+  end
 end
 p min_num
 
 #  5. Start with an array of strings and compute the total length of all the strings.
 #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
 
-strings = ["volleyball", "basketball", "badminton"]
-index = 0
-total_length = 0
-while index < strings.length
-  total_length = total_length + strings[index].length
-  index += 1
+sports = ["volleyball", "basketball", "badminton"]
+length_sum = 0
+sports.each do |sport|
+  length_sum = length_sum + sport.length
 end
-p total_length
+p length_sum
 
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
@@ -80,40 +222,33 @@ items = [
   {name: "plant", price: 7},
   {name: "dresser", price: 50}
 ]
-index = 0 
-lowest_price = items[0][:price]
-while index < items.length
-  if items[index][:price] < lowest_price
-    lowest_price = items[index][:price]
+lowest_price = items[0]
+items.each do |item|
+  if item[:price] < lowest_price[:price]
+    lowest_price = item
   end
-  index += 1
 end
 p lowest_price
 
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
 
-numbers = [12, 6, 22, 1]
-index = 0
+numbers = [2, 5, 12]
 product = 1
-while index < numbers.length
-  product = product * numbers[index]
-  index += 1
+numbers.each do |number|
+  product = product * number
 end
 p product
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
-strings = ["summer", "fall", "winter", "spring"]
-index = 0
-joined = "-"
-while index < strings.length
-  string = strings[index]
-  joined += "#{string}-"
-  index += 1
+sports =["volleyball", "basketball", "badminton"]
+single = "-"
+sports.each do |sport|
+  single = single + "#{sport}-"
 end
-p joined
+p single
 
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
@@ -126,64 +261,24 @@ items = [
   {name: "dresser", price: 50}
 ]
 shortest_name = items[0]
-index = 0
-while index < items.length
-  if items[index][:name].length < shortest_name[:name].length
-    shortest_name = items[index][:name]
+items.each do |item|
+  if item[:name].length < shortest_name[:name].length
+    shortest_name = item
   end
-  index += 1
 end
 p shortest_name
 
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
 
-numbers = [5, 234, 66, 12]
+numbers = [6, 7, 8, 2]
 max_num = numbers[0]
-index = 0
-while index < numbers.length
-  if numbers[index] > max_num
-    max_num = numbers[index]
+numbers.each do |number|
+  if number > max_num
+    max_num = number
   end
-  index += 1
 end
 p max_num
-
-
-
-
-###.each###
-
-#  1. Start with an array of numbers and compute the sum of all the numbers.
-#     For example, [5, 10, 8, 3] becomes 26.
-
-#  2. Start with an array of strings and combine them all into a single string.
-#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
-
-#  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
-
-#  4. Start with an array of numbers and compute the the minumum number.
-#     For example, [5, 10, 8, 3, 9] becomes 3.
-
-#  5. Start with an array of strings and compute the total length of all the strings.
-#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
-
-#  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
-
-#  7. Start with an array of numbers and compute product of all the numbers.
-#     For example, [5, 10, 8, 3] becomes 1200.
-
-#  8. Start with an array of strings and combine them all into a single string, separated by dashes.
-#     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
-
-#  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
-
-# 10. Start with an array of numbers and compute the maximum number.
-#     For example, [5, 10, 8, 3] becomes 10.
-
 
 ###.reduce ###
 
