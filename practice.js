09893d46577097eb5b7ec3730c6f3f0a425dc678
hollymsnell <hollymsnell@gmail.com>
2023-09-15 19:33:04 -0600

    JS concant and interp, feels good, researched window not defined error for user prompt and determined unable to use on server side

--- practice.js
+++ practice.js
@@ -1,71 +1,34 @@
 // node practice.js ***
 
-// # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+// # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-var number = 45;
-if (number === 10) {
-  console.log(0);
-} else {
-  console.log(-1);
+var firstName = "holly";
+var lastName = "snell";
+console.log(firstName + " " + lastName);
+
+// # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+var firstName = "holly";
+var lastName = "snell";
+console.log(`${firstName} ${lastName}`);
+
+// # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+var word = window.prompt("Enter a word:");
+if (word === "marco") {
+  console.log("polo");
 }
 
-// # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+// # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-var nummy = 34;
-if (nummy < 10) {
-  console.log(-1);
-} else if (nummy > 10) {
-  console.log(1);
-} else {
-  console.log(0);
-}
+// # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-// # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+// # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
-var num1 = 3;
-var num2 = 45;
-if (num1 && num2 < 10) {
-  console.log(1);
-} else {
-  console.log(0);
-}
+// # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
-// # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+// # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
-var numero = 6;
-if (numero < 9000) {
-  console.log(-1);
-} else {
-  console.log(1);
-}
+// # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
-// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-
-var digit = 16;
-if (digit < 10) {
-  console.log(9);
-} else if (digit < 20) {
-  console.log(19);
-} else if (digit < 30) {
-  console.log(29);
-} else {
-  console.log(-1);
-}
-
-// # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
-
-num1 = 3;
-num2 = 70;
-if (num1 > 10 || num2 > 10) {
-  console.log(100);
-} else {
-  console.log(-100);
-}
-
-// # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
-
-// # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-
-// # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-
-// # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+// # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
