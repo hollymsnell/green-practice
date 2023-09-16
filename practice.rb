489a8c00c5ef35926fd8b5a6f6ef9d1b47f9933f
hollymsnell <hollymsnell@gmail.com>
2023-09-16 15:17:34 -0600

    practice with while loops ruby

--- practice.rb
+++ practice.rb
@@ -1,81 +1,90 @@
 #ruby practice.rb ***
 
+# 1. Write a while loop to print the numbers 1 through 10.
 
-# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
-
-p "please enter a word"
-word = gets.chomp 
-p word.upcase 
-
-# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
-
-p "Enter a number, bad boy."
-number = gets.chomp
-if number.to_i > 100
-  p "That's a big number, bad boy."
-else
-  p "here's #{number} dollars to spend."
+number = 1
+while number <= 10
+  p number
+  number = number + 1
 end
 
-# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+# 2. Write a while loop that prints the word "hello" 5 times.
 
-p "Enter two numbers, please."
-number1 = gets.chomp
-number2 = gets.chomp
-p number1.to_i + number2.to_i
-
-# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
-
-p "Type a word"
-word = gets.chomp
-p word.reverse 
-
-# 5. Write a program that asks the user to enter a number, then prints the number times 10.
-
-p "enter a number please"
-number = gets.chomp
-p number.to_i * 10
-
-# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
-
-p "Enter two separate words"
-word1 = gets.chomp
-word2 = gets.chomp
-p word1.upcase + " " + word2.upcase
-p "#{word1.upcase} #{word2.upcase}"
-
-# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
-
-p "Enter another word, baby."
-word = gets.chomp
-p "The word #{word} has #{word.length} letters in it."
-
-# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
-
-p "Let's try another number"
-number = gets.chomp
-if number.to_i < 0
-  p "#{number} is a negative number"
-else
-  p "way to stay positive"
+count = 0
+while count < 5
+  p "hello"
+  count = count + 1
 end
 
-# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
-p "Enter two separate numbers, my dearest."
-number1 = gets.chomp
-number2 = gets.chomp
-p "If we add #{number1} and #{number2} the answer will come out to #{number1.to_i + number2.to_i}."
+while true
+  puts "enter a word"
+  word = gets.chomp
+  if word == "stop"
+    break
+  end
+end 
 
-# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-p "Enter the last word"
-word = gets.chomp
-if word.length > 5
-  p "That's a long word"
-else
-  p "that's a small word"
+number = 0
+while number <= 100
+  p number
+  number = number + 5
 end
 
 
+# 5. Write a while loop that prints the number 9000 ten times.
 
+count = 0 
+while count < 10
+  p 9000
+  count = count + 1
+end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+while true
+puts "Enter a number"
+number = gets.chomp
+  if number.to_i > 10
+    break
+  end
+end
+
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+number = 50
+while number <= 70
+  p number
+  number = number + 1
+end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+count = 0
+while count < 144
+  p "Around the world"
+  count = count + 1
+end
+
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+while true
+  puts "Enter a word."
+  word = gets.chomp
+  if word.length > 5
+    break
+  end
+end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+number = 2
+while number <= 40
+  p number
+  number = number + 2
+end
