d7850b81f67c84bf25f1951734eb4291e0afb6c6
hollymsnell <hollymsnell@gmail.com>
2023-09-21 10:44:52 -0600

    .each in ruby array to new array

--- practice.rb
+++ practice.rb
@@ -142,33 +142,112 @@ p to_string
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
+numbers = [1, 4, 45, 3]
+trippled = []
+numbers.each do |number|
+  trippled << number * 3
+end
+p trippled
+
+
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+seasons = ["summer", "fall", "winter", "spring"]
+capitalized = []
+seasons.each do |season|
+  capitalized << season.upcase
+end
+p capitalized
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
+people = [
+  {name: "Holly", age: 32},
+  {name: "Rico", age: 37}
+]
+names = []
+people.each do |person|
+  names << person[:name]
+end
+p names
+
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+numbers = [56, 2, 9, 3, 100]
+added = []
+numbers.each do |number|
+  added << number + 7
+end
+p added
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
+weathers = ["rain", "sunny", "snow", "torrential downpour"]
+number_of_letters = []
+weathers.each do |weather|
+  number_of_letters << weather.length
+end
+p number_of_letters
+
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+people = [
+  {name: "Holly", age: 32},
+  {name: "Rico", age: 37}
+]
+ages = []
+people.each do |person|
+  ages << person[:age]
+end
+p ages
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
+numbers = [13, 55, 6, 123]
+halved = []
+numbers.each do |number|
+  halved << number/2.0
+end
+p halved
+
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
+
+emotions = ["happy", "sad", "afraid", "excited"]
+first_letter = []
+emotions.each do |emotion|
+  first_letter << emotion[0]
+end
+p first_letter
 
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+people = [
+  {name: "Holly", age: 32},
+  {name: "Rico", age: 37}
+]
+doubled_age = []
+people.each do |person|
+  doubled_age << person[:age] * 2
+end
+p doubled_age
+
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
 
+numbers = [123, 1, 2, 3]
+strings = []
+numbers.each do |number|
+  strings << number.to_s 
+end
+p strings
 
 #***.map***
 
