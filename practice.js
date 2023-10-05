3d27d081f6ec95bb06f5dfe340ea7da933fb05bb
hollymsnell <hollymsnell@gmail.com>
2023-10-05 15:01:11 -0600

    loop within loop javascript 1-5 challenging

--- practice.js
+++ practice.js
@@ -1,151 +1,118 @@
 // node practice.js ***
 
-// #  1. Convert an array of arrays into a hash.
-// #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+// #  1. Use a nested loop to convert an array of number pairs into a single flattened array.
+// #     For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
 
-var pairs = [
+var numberPairs = [
   [1, 3],
   [8, 9],
   [2, 16],
 ];
-var pairsObject = {};
+var flattened = [];
 var index = 0;
-while (index < pairs.length) {
-  var key = pairs[index][0];
-  var value = pairs[index][1];
-  pairsObject[key] = value;
-  index += 1;
-}
-console.log(pairsObject);
-
-// #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
-// #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
-
-var items = [
-  { id: 1, color: "blue", price: 32 },
-  { id: 2, color: "red", price: 12 },
-];
-var itemsObject = {};
-index = 0;
-while (index < items.length) {
-  itemsObject[items[index].id] = items[index];
-  index += 1;
-}
-console.log(itemsObject);
-
-// #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
-// #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
-
-var word = "bookkeeper";
-var letterFrequencies = {};
-index = 0;
-while (index < word.length) {
-  var letter = word[index];
-  if (letterFrequencies[letter] === undefined) {
-    letterFrequencies[letter] = 0;
+while (index < numberPairs.length) {
+  var numberPair = numberPairs[index];
+  var index2 = 0;
+  while (index2 < numberPair.length) {
+    var number = numberPair[index2];
+    flattened.push(number);
+    index2 += 1;
   }
-  letterFrequencies[letter] += 1;
   index += 1;
 }
-console.log(letterFrequencies);
+console.log(flattened);
 
-// #  4. Convert a hash into an array of arrays.
-// #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+// #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
+// #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
 
-var items = { chair: 100, book: 14 };
-var array = [];
-Object.entries(items).forEach(function ([name, price]) {
-  array.push(name, price);
-});
-console.log(array);
-
-// #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
-// #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
-
-var people = { 321: { name: "Alice", age: 31 }, 322: { name: "Maria", age: 27 } };
-var peopleArray = [];
-Object.entries(people).forEach(function ([id, person]) {
-  person.id = parseInt(id);
-  peopleArray.push(person);
-});
-console.log(peopleArray);
-
-// #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
-// #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
-
-var words = ["do", "or", "do", "not"];
-var wordFrequencies = {};
+var letters1 = ["a", "b", "c"];
+var letters2 = ["d", "e", "f", "g"];
+var combined = [];
 index = 0;
-while (index < words.length) {
-  var word = words[index];
-  if (wordFrequencies[word] === undefined) {
-    wordFrequencies[word] = 0;
+while (index < letters1.length) {
+  index2 = 0;
+  while (index2 < letters2.length) {
+    combined.push(letters1[index] + letters2[index2]);
+    index2 += 1;
   }
-  wordFrequencies[word] += 1;
-  index++;
-}
-console.log(wordFrequencies);
-
-// #  7. Convert a hash into a flat array containing all the hash’s keys and values.
-// #     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
-
-var hash = { a: 1, b: 2, c: 3, d: 4 };
-var flattenedArray = [];
-Object.entries(hash).forEach(function ([key, value]) {
-  flattenedArray.push(key);
-  flattenedArray.push(value);
-});
-console.log(flattenedArray);
-
-// #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
-// #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
-
-var priceObject = { chair: 75, book: 15 };
-var items = [
-  { name: "chair", color: "red", weight: 10 },
-  { name: "book", color: "black", weight: 1 },
-];
-var combinedObject = {};
-var index = 0;
-while (index < items.length) {
-  var item = items[index];
-  var name = item.name;
-  var color = item.color;
-  var weight = item.weight;
-  var price = priceObject[name];
-  combinedObject[name] = { price: price, color: color, weight: weight };
   index += 1;
 }
-console.log(combinedObject);
+console.log(combined);
 
-// #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
-// #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+// #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+// #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
-var books = [
-  { author: "Jeff Smith", title: "Bone" },
-  { author: "George Orwell", title: "1984" },
-  { author: "Jeff Smith", title: "RASL" },
-];
-var booksObject = {};
-var index = 0;
-while (index < books.length) {
-  var book = books[index];
-  var author = book.author;
-  var title = book.title;
-  if (booksObject[author] === undefined) {
-    booksObject[author] = [];
+var letters = ["a", "b", "c", "d"];
+var combinations = [];
+index = 0;
+while (index < letters.length) {
+  index2 = 0;
+  while (index2 < letters.length) {
+    if (index != index2) {
+      combinations.push(letters[index] + letters[index2]);
+    }
+    index2 += 1;
   }
-  booksObject[author].push(title);
   index += 1;
 }
-console.log(booksObject);
+console.log(combinations);
 
-// # 10. Given a hash, create a new hash that has the keys and values switched.
-// #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
+// #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
+// #     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
 
-var originalObject = { a: 1, b: 2, c: 3 };
-var flippedObject = {};
-Object.entries(originalObject).forEach(function ([key, value]) {
-  flippedObject[value] = key;
-});
-console.log(flippedObject);
+var numbers = [5, -2, 1, -9, -7, 2, 6];
+var largestProduct = numbers[0] * numbers[1];
+index = 0;
+while (index < numbers.length) {
+  currentNumber = numbers[index];
+  index2 = 0;
+  while (index2 < numbers.length) {
+    if (index !== index2) {
+      var otherNumber = numbers[index2];
+      var product = currentNumber * otherNumber;
+      if (product > largestProduct) {
+        largestProduct = product;
+      }
+    }
+    index2 += 1;
+  }
+  index += 1;
+}
+console.log(largestProduct);
+
+// #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
+// #     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
+
+var numberPairs = [
+  [1, 3],
+  [8, 9],
+  [2, 16],
+];
+var sum = 0;
+index = 0;
+while (index < numberPairs.length) {
+  var numberPair = numberPairs[index];
+  index2 = 0;
+  while (index2 < numberPair.length) {
+    var number = numberPair[index2];
+    sum = sum + number;
+    index2 += 1;
+  }
+  index += 1;
+}
+console.log(sum);
+
+// #  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
+// #     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
+
+// #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
+// #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
+
+// #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
+// #     For example, [1, 8, 3, 10] becomes 18.
+
+// #  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+// #     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+
+// # 10. Use a nested loop to convert an array of string arrays into a single string.
+// #     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
