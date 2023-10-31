23d6c6a5b53c59f80ec64855b15a2550175f3b0f
hollymsnell <hollymsnell@gmail.com>
2023-10-31 12:07:01 -0600

    review of conditionals in JS and ruby

--- practice.js
+++ practice.js
@@ -1,151 +1,55 @@
 // node practice.js ***
 
-// #  1. Convert an array of arrays into a hash.
-// #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+// # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-var pairs = [
-  [1, 3],
-  [8, 9],
-  [2, 16],
-];
-var pairsObject = {};
-var index = 0;
-while (index < pairs.length) {
-  var key = pairs[index][0];
-  var value = pairs[index][1];
-  pairsObject[key] = value;
-  index += 1;
+var number = 34;
+if (number === 10) {
+  console.log(0);
+} else {
+  console.log(-1);
 }
-console.log(pairsObject);
 
-// #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
-// #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+// # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-var items = [
-  { id: 1, color: "blue", price: 32 },
-  { id: 2, color: "red", price: 12 },
-];
-var itemsObject = {};
-var index = 0;
-while (index < items.length) {
-  itemsObject[items[index].id] = items[index];
-  index += 1;
+// # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+var number1 = 45;
+var number2 = 2;
+if (number1 < 10 && number2 < 10) {
+  console.log(1);
+} else {
+  console.log(0);
 }
-console.log(itemsObject);
 
-// #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
-// #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+// # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-var word = "bookkeeper";
-var letterFrequencies = {};
-var index = 0;
-while (index < word.length) {
-  var letter = word[index];
-  if (letterFrequencies[letter] === undefined) {
-    letterFrequencies[letter] = 0;
-  }
-  letterFrequencies[letter] += 1;
-  index += 1;
+// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+var number = 34;
+if (number < 10) {
+  console.log(9);
+} else if (number < 20) {
+  console.log(19);
+} else if (number < 30) {
+  console.log(29);
+} else {
+  console.log(-1);
 }
-console.log(letterFrequencies);
 
-// #  4. Convert a hash into an array of arrays.
-// #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+// # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
-var things = { chair: 100, book: 14 };
-var namePricePairs = [];
-Object.entries(things).forEach(function ([name, price]) {
-  namePricePairs.push(name, price);
-});
-console.log(namePricePairs);
-
-// #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
-// #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
-
-var people = { 321: { name: "Alice", age: 31 }, 322: { name: "Maria", age: 27 } };
-var peopleArray = [];
-Object.entries(people).forEach(function ([id, person]) {
-  person.id = parseInt(id);
-  peopleArray.push(person);
-});
-console.log(peopleArray);
-
-// #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
-// #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
-
-var words = ["do", "or", "do", "not"];
-var wordFrequencies = {};
-var index = 0;
-while (index < words.length) {
-  var word = words[index];
-  if (wordFrequencies[word] === undefined) {
-    wordFrequencies[word] = 0;
-  }
-  wordFrequencies[word] += 1;
-  index++;
+var number1 = 23;
+var number2 = 6;
+if (number1 > 10 || number2 > 10) {
+  console.log(100);
+} else {
+  console.log(-100);
 }
-console.log(wordFrequencies);
 
-// #  7. Convert a hash into a flat array containing all the hash’s keys and values.
-// #     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+// # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
-var hash = { a: 1, b: 2, c: 3, d: 4 };
-var flattenedArray = [];
-Object.entries(hash).forEach(function ([key, value]) {
-  flattenedArray.push(key);
-  flattenedArray.push(value);
-});
-console.log(flattenedArray);
+// # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
-// #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
-// #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+// # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
-var priceObject = { chair: 75, book: 15 };
-var items = [
-  { name: "chair", color: "red", weight: 10 },
-  { name: "book", color: "black", weight: 1 },
-];
-var combinedObject = {};
-var index = 0;
-while (index < items.length) {
-  var item = items[index];
-  var name = item.name;
-  var color = item.color;
-  var weight = item.weight;
-  var price = priceObject[name];
-  combinedObject[name] = { price: price, color: color, weight: weight };
-  index++;
-}
-console.log(combinedObject);
-
-// #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
-// #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
-
-var books = [
-  { author: "Jeff Smith", title: "Bone" },
-  { author: "George Orwell", title: "1984" },
-  { author: "Jeff Smith", title: "RASL" },
-];
-var booksObject = {};
-var index = 0;
-while (index < books.length) {
-  var book = books[index];
-  var author = book.author;
-  var title = book.title;
-  if (booksObject[author] === undefined) {
-    booksObject[author] = [];
-  }
-  booksObject[author].push(title);
-  index++;
-}
-console.log(booksObject);
-
-// # 10. Given a hash, create a new hash that has the keys and values switched.
-// #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
-
-var ogObject = { a: 1, b: 2, c: 3 };
-var flipped = {};
-Object.entries(ogObject).forEach(function ([key, value]) {
-  flipped[value] = key;
-});
-console.log(flipped);
+// # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
--- practice.rb
+++ practice.rb
@@ -1,141 +1,64 @@
 #ruby practice.rb ***
 
 
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-#  1. Convert an array of arrays into a hash.
-#     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
-
-pairs = [[1, 3], [8, 9], [2, 16]]
-pairs_hash = {}
-index = 0
-while index < pairs.length
-  key = pairs[index][0]
-  value = pairs[index][1]
-  pairs_hash[key] = value
-  index += 1
+number = 5
+if number == 10 
+  p 0
+else
+  p -1
 end
-p pairs_hash
 
-#  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
-#     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-items = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] 
-items_hash = {}
-index = 0
-while index < items.length
-  items_hash[items[index][:id]] = items[index]
-  index += 1
+number = 1
+if number < 10
+  p -1
+elsif number > 10
+  p 1
+else
+  p 0
 end
-p items_hash
 
-#  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
-#     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
-word = "bookkeeper"
-letter_frequencies = {}
-index = 0
-while index < word.length
-  letter = word[index]
-  if letter_frequencies[letter] == nil
-    letter_frequencies[letter] = 0
-  end
-  letter_frequencies[letter] += 1
-  index +=1
+number1 = 3
+number2 = 4
+if number1 && number2 < 10
+  p 1
+else 
+  p 0
 end
-p letter_frequencies
 
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-#  4. Convert a hash into an array of arrays.
-#     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
-
-items = {"chair" => 100, "book" => 14}
-item_pairs = []
-items.each do |name, price|
-  item_pairs << [name, price]
+number = 800000
+if number > 9000
+  p 1
+else
+  p -1
 end
-p item_pairs
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
 
-#  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
-#     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
 
-people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
-people_array = []
-people.each do |id, person|
-  person[:id] = id
-  people_array << person
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+number = 234
+number2 = 3
+if number || number2 > 10
+  p 100
+else
+  p -100
 end
-p people_array
 
-#  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
-#     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
 
-words = ["do", "or", "do", "not"]
-word_frequencies = {}
-index = 0
-while index < words.length
-  word = words[index]
-  if word_frequencies[word] == nil
-    word_frequencies[word] = 0
-  end
-  word_frequencies[word] += 1
-  index += 1
-end
-p word_frequencies
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
-#  7. Convert a hash into a flat array containing all the hash’s keys and values.
-#     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
-hash = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}
-flattened_array = []
-hash.each do |key, value|
-  flattened_array << key
-  flattened_array << value
-end
-p flattened_array
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
-#  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
-#     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
-
-price_hash = {"chair" => 75, "book" => 15}
-items = [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] 
-combined_hash = {}
-index = 0
-while index < items.length
-  item = items[index]
-  name = item[:name]
-  color = item[:color]
-  weight = item[:weight]
-  price = price_hash[name]
-  combined_hash[name] = { price: price, color: color, weight: weight }
-  index += 1
-end
-p combined_hash
-
-#  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
-#     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
-
-books = [{ author: "Jeff Smith", title: "Bone" }, { author: "George Orwell", title: "1984" }, { author: "Jeff Smith", title: "RASL" }]
-books_hash = {}
-index = 0
-while index < books.length
-  book = books[index] 
-  author = book[:author]
-  title = book[:title]
-  if books_hash[author] == nil
-    books_hash[author] = []
-  end
-  books_hash[author] << title
-  index += 1
-end
-p books_hash
-
-# 10. Given a hash, create a new hash that has the keys and values switched.
-#     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
-
-og_hash = {"a" => 1, "b" => 2, "c" => 3}
-flipped_hash = {}
-og_hash.each do |key, value|
-  flipped_hash[value] = key
-end
-p flipped_hash
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
