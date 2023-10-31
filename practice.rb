e557f25fcdf55c29c108612fa2ae95c8a9a3dc57
hollymsnell <hollymsnell@gmail.com>
2023-10-31 15:14:09 -0600

    review of interp and concant, and gets.chomp

--- practice.rb
+++ practice.rb
@@ -1,64 +1,56 @@
 #ruby practice.rb ***
 
 
-# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-number = 5
-if number == 10 
-  p 0
+first_name = "Holly"
+last_name = "Berry"
+p "My name is " + first_name + " " + last_name + ", nice to meet you." 
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first = "Joe"
+last = "Richards"
+p "His name is #{first} #{last} and he's trouble."
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+puts "Enter a word"
+word = gets.chomp
+if word == "marco"
+  p "#{word} polo"
 else
-  p -1
+  p "thanks for the word" 
 end
 
-# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-number = 1
-if number < 10
-  p -1
-elsif number > 10
-  p 1
-else
-  p 0
-end
+color1 = "red"
+color2 = "green"
+color3 = "blue"
+p "Common Christmas colors are " + color1 + ", " + color2 + ", " + "and sometimes " + color3 + "."
 
-# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-number1 = 3
-number2 = 4
-if number1 && number2 < 10
-  p 1
+color1 = "red"
+color2 = "green"
+color3 = "blue"
+p "Common Christmas colors are #{color1}, #{color2}, and sometimes #{color3}."
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+puts "Enter your password"
+password = gets.chomp
+if password == "Joshua"
+  p "Shall we play a game?"
 else 
-  p 0
+  p "Access Denied"
 end
 
-# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-
-number = 800000
-if number > 9000
-  p 1
-else
-  p -1
-end
-
-# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-
-
-
-# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
-
-number = 234
-number2 = 3
-if number || number2 > 10
-  p 100
-else
-  p -100
-end
-
-
-# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
-
-# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-
-# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-
-# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
