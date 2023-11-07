8be3fb41d92f70c3561ebe2ef16982ccd7d7e91c
hollymsnell <hollymsnell@gmail.com>
2023-11-07 15:34:26 -0700

    practice with simple functions in ruby

--- practice.rb
+++ practice.rb
@@ -1,55 +1,81 @@
 #ruby practice.rb ***
 
-# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-person = {"first_name" => "holly", "last_name" => "snell", "email" => "email@email.com"}
-p person["first_name"]
-p person["last_name"]
-p person["email"]
+def get_double(number)
+  return number * 2
+end
 
-# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+p get_double(14)
 
-people = [
-  {"first_name" => "Joy", "last_name" => "Winters"},
-  {"first_name" => "Benny", "last_name" => "Bates"}
-]
-p people[0]["first_name"]
-p people[0]["last_name"]
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
+def capitalize(string)
+  return string.upcase
+end
 
-# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+p capitalize("happy birthday")
 
-menu = {"cheese" => 5, "cream" => 7, "tacos" => 6}
-menu["burger"] = 10
-p menu
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
-# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+def subtract(number1, number2)
+  return number1 - number2
+end
 
-book = {"title" => "Homeward Bound", "author" => "Billy Beans", "number_of_pages" => 78, "language" => "French"}
-p book["title"]
-p book["author"]
-p book["number_of_pages"]
-p book["language"]
+p subtract(800, 798)
 
-# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
-books = [
-  {"title" => "Homeward Bound", "author" => "Billy Beans", "number_of_pages" => 78, "language" => "French"},
-  {"title" => "Benz", "author" => "Jolly Williams", "number_of_pages" => 800, "language" => "Finnish"},
-  {"title" => "Select Class", "author" => "Jake Tass", "number_of_pages" => 348, "language" => "Italian"}
-]
-p books[2]["author"]
+def squared(number)
+  return number * number
+end
 
-# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+p squared(4)
 
-states = {"Colorado" => "Denver", "New Mexico" => "Santa Fe", "North Dakota" => "Bismark"}
-states["Hawaii"] = "Honolulu"
-p states
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+def first_letter(string)
+  return string[0]
+end
 
-# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+p first_letter("xavier")
 
-# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
-# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+def combine(string1, string2, string3)
+  return "#{string1} #{string2} #{string3}"
+end
+
+p combine("can", "I", "come")
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def to_string(number)
+  return number.to_s 
+end
+
+p to_string(67)
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+def repeat(string)
+  return string * 5
+end
+
+p repeat("get lost")
+
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+def average(number1, number2, number3)
+  return (number1 + number2 + number3) / 3
+end
+ 
+p average(45, 6, 29)
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def add_thirty(number)
+  return number + 30
+end
+
+p add_thirty(45)
