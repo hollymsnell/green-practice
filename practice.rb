7d1f403dc02ba6503d46241c90bb7331aa8046d7
hollymsnell <hollymsnell@gmail.com>
2023-11-07 10:45:33 -0700

    hash practice in ruby

--- practice.rb
+++ practice.rb
@@ -1,53 +1,55 @@
 #ruby practice.rb ***
 
-# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
-array = ["joy", "love", "happy"]
-array << "wonder"
-array << "success"
-p array
+person = {"first_name" => "holly", "last_name" => "snell", "email" => "email@email.com"}
+p person["first_name"]
+p person["last_name"]
+p person["email"]
 
-# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
 
-letters = ["a", "g", "h", "y"]
-letters[1] = 4
-p letters
-
-# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
-
-numbers = [1, 5, 7, 8]
-index = 0
-while index < numbers.length
-  p numbers[index]
-  index += 1
-end
+people = [
+  {"first_name" => "Joy", "last_name" => "Winters"},
+  {"first_name" => "Benny", "last_name" => "Bates"}
+]
+p people[0]["first_name"]
+p people[0]["last_name"]
 
 
-# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
-# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+menu = {"cheese" => 5, "cream" => 7, "tacos" => 6}
+menu["burger"] = 10
+p menu
 
-strings = ["winter", "summer", "spring", "fall"]
-strings[2] = strings[2].upcase
-p strings
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
-# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+book = {"title" => "Homeward Bound", "author" => "Billy Beans", "number_of_pages" => 78, "language" => "French"}
+p book["title"]
+p book["author"]
+p book["number_of_pages"]
+p book["language"]
 
-names = ["Adam", "Oliver", "Holly"]
-index = 0
-while index < names.length
-  p names[index]
-  index += 1
-end
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
-# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+books = [
+  {"title" => "Homeward Bound", "author" => "Billy Beans", "number_of_pages" => 78, "language" => "French"},
+  {"title" => "Benz", "author" => "Jolly Williams", "number_of_pages" => 800, "language" => "Finnish"},
+  {"title" => "Select Class", "author" => "Jake Tass", "number_of_pages" => 348, "language" => "Italian"}
+]
+p books[2]["author"]
 
-# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
-# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+states = {"Colorado" => "Denver", "New Mexico" => "Santa Fe", "North Dakota" => "Bismark"}
+states["Hawaii"] = "Honolulu"
+p states
 
-# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
-countries = ["China", "Russia", "Argentina"]
-countries << "Chile"
-p countries
+# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
