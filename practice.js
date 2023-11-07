2a867e099d180f1e29934093df8b78426150c390
hollymsnell <hollymsnell@gmail.com>
2023-11-07 15:19:37 -0700

    hash practice in JS

--- practice.js
+++ practice.js
@@ -1,40 +1,49 @@
 // node practice.js ***
 
-// # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+// # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
-var array = ["boy", "girl", "chica"];
-array.push("chico");
-console.log(array);
+var person = { firstName: "Holly", lastName: "Snell", email: "email@email.com" };
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.email);
 
-// # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+// # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
 
-var letters = ["e", "y", "e", "s"];
-letters[1] = "r";
-console.log(letters);
+var people = [
+  { firstName: "Joy", lastName: "Williams" },
+  { firstName: "Bob", lastName: "The Builder" },
+  { firstName: "Jane", lastName: "Doe" },
+];
+console.log(people[0].firstName);
+console.log(people[0].lastName);
 
-// # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+// # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
-var numbers = [6, 7, 34, 100, 4];
-var index = 0;
-while (index < numbers.length) {
-  console.log(numbers[index]);
-  index++;
-}
+var menu = { pie: 5, cremepuff: 6, fondue: 19 };
+menu["cake"] = 7;
+console.log(menu);
 
-// # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+// # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
-// # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+// # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
-var strings = ["don't", "get", "mad"];
-strings[2] = strings[2].toUpperCase();
-console.log(strings);
+var books = [
+  { title: "book1", author: "author1" },
+  { title: "book2", author: "author2" },
+  { title: "book3", author: "author3" },
+];
+console.log(books[2].author);
 
-// # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+// # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
-// # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+var capitals = { denver: "colorado", trenton: "new jersey", pierre: "south dakota" };
+capitals["concord"] = "new hampshire";
+console.log(capitals);
 
-// # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+// # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
-// # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+// # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
-// # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+// # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+// # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
