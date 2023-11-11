7ac6f32902d6849bbf2cba483963e552e826524c
hollymsnell <hollymsnell@gmail.com>
2023-11-11 10:04:51 -0700

    .map practice in ruby, quick/easy

--- practice.rb
+++ practice.rb
@@ -214,23 +214,62 @@ p ages
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
 
+
+#***.map**
+
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
+
+numbers = [5, 4, 7]
+times_three = numbers.map do |number|
+  number * 3
+end
+p times_three
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
+strings = ["rico", "suave"]
+capitalized = strings.map do |string|
+  string.upcase
+end
+p capitalized
+
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+names = people.map do |person|
+  person[:name]
+end
+p names
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
+numbers = [2, 23, 8]
+added_seven = numbers.map do |number|
+  number + 7
+end
+p added_seven
+
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
+strings = ["heaven", "hell"]
+string_lengths = strings.map do |string|
+  string.length
+end
+p string_lengths
+
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+ages = people.map do |person|
+  person[:age]
+end
+p ages
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
